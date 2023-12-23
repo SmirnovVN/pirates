04ddb5cd259c2bb517c65a3f7710659b67be395d
DESKTOP-OMVJ0FP\ausov <an.usov2011@gmail.com>
2023-12-23 16:16:44 +0100

    get_map implemented

--- app/entities/map.py
+++ app/entities/map.py
@@ -1,2 +1,11 @@
+from typing import List
+
+from app.entities.island import Island
+
+
 class Map:
-    pass
+    def __init__(self, width: int, height: int, slug: str, islands: List[Island]):
+        self.width = width
+        self.height = height
+        self.slug = slug
+        self.islands = islands
--- app/service/game_service.py
+++ app/service/game_service.py
@@ -1,30 +1,58 @@
+import asyncio
 from typing import List
 
+import httpx
+
+from app.config import settings
 from app.entities.map import Map
+from app.entities.ship import Ship
 from app.schemas.response import Response
 from app.schemas.scan import Scan
-from app.entities.ship import Ship
 
 
-class GameService:
+async def get_map() -> Map:
+    headers = {"X-API-Key": settings.token}
+    async with httpx.AsyncClient() as client:
+        response = await client.get(f"{settings.external_url}/map", headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        if data.get('success'):
+            map_url = data.get('mapUrl')
+            print(data)
+            async with httpx.AsyncClient() as client:
+                map_response = await client.get(map_url, headers=headers)
+            if map_response.status_code == 200:
+                map_data = map_response.json()
+                print(map_data)
+                return Map(**map_data)
+            else:
+                print(f"Request failed with status code {map_response.status_code}")
+    else:
+        print(f"Request failed with status code {response.status_code}")
 
-    def register_deathmatch(self) -> bool:
-        pass
 
-    def leave_deathmatch(self) -> bool:
-        pass
+asyncio.run(get_map())
 
-    def register_battle_royal(self) -> bool:
-        pass
 
-    def get_map(self) -> Map:
-        pass
+async def register_deathmatch() -> bool:
+    pass
 
-    def scan(self) -> Scan:
-        pass
 
-    def long_scan(self) -> Scan:
-        pass
+async def leave_deathmatch() -> bool:
+    pass
 
-    def send_commands(self, ships: List[Ship]) -> Response:
-        pass
+
+async def register_battle_royal() -> bool:
+    pass
+
+
+async def scan() -> Scan:
+    pass
+
+
+async def long_scan() -> Scan:
+    pass
+
+
+async def send_commands(ships: List[Ship]) -> Response:
+    pass
