8849d37aa4e97efe034fe9e770acd3a0c15b9bc4
DESKTOP-OMVJ0FP\ausov <an.usov2011@gmail.com>
2023-12-23 22:07:07 +0100

    added new directions and shooting calculations

--- app/main.py
+++ app/main.py
@@ -4,10 +4,12 @@ import sys
 import uvicorn
 from app.api.routes import router
 from app.config import settings
-from fastapi import FastAPI
+from fastapi import FastAPI, BackgroundTasks
+
+from app.entities.game import Game
+from app.service.game_service import scan
 
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG if settings.debug else logging.INFO)
-
 
 app = FastAPI()
 
@@ -17,8 +19,16 @@ async def root():
     return {"message": "Hello World"}
 
 
-app.include_router(router)
+@app.on_event("startup")
+async def startup_event(background_tasks: BackgroundTasks):
+    print("Startup")
+    res = await scan()
+    if res:
+        game = Game()
+        await game.start(background_tasks)
 
+
+app.include_router(router)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", reload=True, port=8000)
