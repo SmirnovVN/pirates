5b867b0bc814a35afb53b25edbe0bfdedea57b2d
VNSmirnov <VNSmirnov@sportmaster.ru>
2023-12-23 19:10:47 +0300

    game class

--- app/entities/game.py
+++ app/entities/game.py
@@ -1,2 +1,26 @@
-class Game:
-    pass
+from typing import Optional, List
+
+from app.entities.map import Map
+from app.entities.ship import Ship
+from app.service.game_service import get_map, scan
+from app.utils.singleton import Singleton
+
+
+class Game(metaclass=Singleton):
+    def __init__(self):
+        self.game_map: Optional[Map] = None
+        self.ships: List[Ship] = []
+        self.enemies: List[Ship] = []
+        self.started = False
+
+    @staticmethod
+    def stop():
+        game = Game()
+        game.started = False
+        Game._instances = {}
+
+    async def start(self):
+        self.game_map = await get_map()
+        s = scan()
+        # TODO run forever
+        self.started = True
---
+++ app/enums/game_type.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+
+class GameType(Enum):
+    BATTLE_ROYAL = 'battle_royal'
+    DEATHMATCH = 'deathmatch'
